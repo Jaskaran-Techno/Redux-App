36d8d65bbeb7c7f2d31f37dba25044dfdf40df6e
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2025-02-18 14:54:42 +0530

    Redux App

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,9 +18,6 @@ const Navbar = () => {
           <a className="nav-link" href="#">Link</a>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="#">Action</a></li>
             <li><a className="dropdown-item" href="#">Another action</a></li>
